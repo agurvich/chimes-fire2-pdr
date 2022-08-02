00775e643b0f6883fd5458b54744660a9022ebe2
Alex Gurvich <agurvich@u.northwestern.edu>
2022-08-02 14:55:58 -0500

    remove line referenced quest restartdisktests

--- job_maker.sh
+++ job_maker.sh
@@ -6,9 +6,7 @@ this_command=$1
 ## job preamble with allocation, etc...
 preamble=${HOME}/ss/preamble.sh
 
-## target directory whose simulations we should loop over and generate halo files for
-sim_path=/projects/b1026/snapshots/restartdisktest/m11a_m2e3
-
+## simulations we should loop over
 names=(m09_res30 m10q_res30 m10v_res30 m11b_res2100 m11d_res7100 m11e_res7100 m11h_res7100 m11i_res7100 m11q_res880 m12m_res7100 m12z_res4200 m12b_res7100 m12c_res7100 m12f_res7100 m12_elvis_RomeoJuliet_res3500 m12_elvis_ThelmaLouise_res4000 m12i_res7100 m12r_res7100 m12w_res7100 m12_elvis_RomulusRemus_res4000)
 
 #for sim_dir in ${sim_path}/*
