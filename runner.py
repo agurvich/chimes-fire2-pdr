02b70ebb31f48fe2e42bc9bb358eee554ff295b1
Alex Gurvich <agurvich@u.northwestern.edu>
2022-08-08 17:51:08 -0500

    it runs!

--- runner.py
+++ runner.py
@@ -2,28 +2,33 @@ import os
 import sys
 import getopt
 import multiprocessing
+import time
+import ctypes as ct
 
 from abg_python.galaxy.gal_utils import Galaxy
 
 def main(savename='m09_res30',suite_name='metal_diffusion',mps=1,**kwargs):
 
-
-    
     ## directories for reading in from and outputting new files to
     output_dir = os.path.join(os.environ['HOME'],'scratch','data',suite_name,savename,'chimes')
     
     ## snapshots corresponding to the FIRE-2 public data release
-    snaps = [600, 277, 172, 120, 88][::-1]
+    snaps = [88]#[600, 277, 172, 120, 88][::-1]
 
     if mps is None: mps = multiprocessing.cpu_count()
+    
+    print(f'using {mps} threads')
+    time.sleep(3)
 
     for snap in snaps: 
-        galaxy = Galaxy(savename,snap,suite_name=suite_name,loud=False,load_halo_file=False,loud_metadata=False)
+        galaxy = Galaxy(savename,snap,suite_name=suite_name,loud=False,full_init=False,loud_metadata=False)
         try: produce_chimes_output(snap,galaxy.snapdir,output_dir,mps=mps)
-        except Exception as e: print(f"{snap} failed: {repr(e)}")
+        except Exception as e: 
+            print(f"{snap} failed: {repr(e)}")
+            raise
         
 def produce_chimes_output(
-    snap,
+    snapnum,
     snapdir,
     output_dir,
     mps=1,
@@ -31,19 +36,27 @@ def produce_chimes_output(
     group_output_snapdir=True):
 
     ## get an array of relevant snapshot files
-    input_file = locate_snapshot_files(snapdir,snap)
+    input_file = locate_snapshot_files(snapdir,snapnum)
 
     ## creates an output directory if requested and necessary
-    if os.path.isdir(input_file): 
-        raise NotImplementedError(f"We don't know how to read in {len(os.listdir(input_file))} snapshots for the ISRF yet")
-        output_dir = os.path.join(output_dir,f'snapdir_{snap:03d}')
+
     if not os.path.isdir(output_dir): os.makedirs(output_dir)
+
     output_file = os.path.join(
         output_dir,
-        "chimes_"+os.path.basename(input_file))
+        f'chimes_snapshot_{int(snapnum):03d}.hdf5')
 
     ## generate a parameter file, save it in the datadir
     param_file = create_param_file(input_file,output_file)
+
+    
+    ## attempt to import chimes, get an error before we do anything heavy
+    try:
+        chimesLib = ct.cdll.LoadLibrary(
+            os.path.join(os.environ['WORK'],'CHIMES-repos','chimes','libchimes.so'))
+    except:
+        print("failed to import chimes, make sure it's built")
+        raise
 
     ## change directories to the chimes driver location
     if chimes_driver_dir is None: 
@@ -51,13 +64,19 @@ def produce_chimes_output(
         os.environ['WORK'],
         'CHIMES-repos',
         'chimes-driver')
-    os.chdir(chimes_driver_dir)
 
-    ## determine what we're running
-    if mps <= 1: exec_str = f"python chimes-driver.py {param_file}"
-    else: exec_str = f"mpirun -np {mps} python chimes-driver.py {param_file}"
+    current_dir = os.getcwd()
+    try:
+        os.chdir(chimes_driver_dir)
 
-    os.system(exec_str)
+        ## determine what we're running
+        if mps <= 1: exec_str = f"python chimes-driver.py {param_file}"
+        else: exec_str = f"mpirun -np {mps} python chimes-driver.py {param_file}"
+
+        os.system(exec_str)
+
+    except: raise
+    finally: os.chdir(current_dir)
 
 
 def locate_snapshot_files(snapdir,snap):
@@ -94,7 +113,8 @@ def create_param_file(input_file,output_file):
     ##    output_file                <OUTPUT_FILE>
     template = template.replace("<OUTPUT_FILE>",output_file)
 
-    param_file = output_file.replace('.hdf5','.param')
+    if '.hdf5' in output_file: param_file = output_file.replace('.hdf5','.param')
+    else: param_file = output_file + '.param'
     with open(param_file,'w') as handle: handle.write(template)
 
     return param_file
@@ -105,7 +125,7 @@ if __name__ == '__main__':
         argv,'',[
         'savename=',
         'suite_name=',
-        'dXkpc='])
+        'mps='])
     #options:
     #--snap(low/high) : snapshot numbers to loop through
     #--savename : name of galaxy to use
